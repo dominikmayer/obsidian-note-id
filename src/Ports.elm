5ecad8e9383f82b7f02ba80bdc672c0c350a4f32
Dominik Mayer <dominik.mayer@gmail.com>
2025-01-25 22:19:17 +0700

    Cleaned up

--- src/Ports.elm
+++ src/Ports.elm
@@ -1,13 +1,7 @@
 port module Ports exposing (..)
 
-import Json.Decode as Decode
-import Json.Encode as Encode
-
 
 port openFile : String -> Cmd msg
-
-
-port sendNotes : List Encode.Value -> Cmd msg
 
 
 port receiveNotes : (List NoteMeta -> msg) -> Sub msg
@@ -21,19 +15,3 @@ type alias NoteMeta =
     , id : Maybe String
     , filePath : String
     }
-
-
-decodeNote : Decode.Decoder NoteMeta
-decodeNote =
-    Decode.map3 NoteMeta
-        (Decode.field "title" Decode.string)
-        (Decode.field "id" (Decode.string |> Decode.andThen decodeMaybeId))
-        (Decode.field "filePath" Decode.string)
-
-
-decodeMaybeId : String -> Decode.Decoder (Maybe String)
-decodeMaybeId id =
-    if id == "null" then
-        Decode.succeed Nothing
-    else
-        Decode.succeed (Just id)
