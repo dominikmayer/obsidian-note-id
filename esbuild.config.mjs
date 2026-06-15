5109e4a146b4898b8d40d8c52d8c4313c247baf9
Dominik Mayer <dominik.mayer@gmail.com>
2026-06-15 21:59:33 +0700

    Replaced "builtin-modules"

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -1,6 +1,6 @@
 import esbuild from "esbuild";
 import process from "process";
-import builtins from "builtin-modules";
+import { builtinModules } from "node:module";
 import elmPlugin from "esbuild-plugin-elm";
 import path from "path";
 import fs from "fs";
@@ -34,7 +34,7 @@ const context = await esbuild.context({
 		"@lezer/common",
 		"@lezer/highlight",
 		"@lezer/lr",
-		...builtins],
+		...builtinModules],
 	format: "cjs",
 	target: "es2018",
 	logLevel: "info",
